12cd5ff829ec88602a58c6d1b415074ebd95f2f8
jacbarc <108655243+jacbarc@users.noreply.github.com>
2022-07-05 19:34:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/samochod-ortal.jpg') {
-      myImage.setAttribute('src','images/ortal-logo.jpg');
+    if(mySrc === '/samochod-ortal.jpg') {
+      myImage.setAttribute('src','/ortal-logo.jpg');
     } else {
-      myImage.setAttribute('src','images/samochod-ortal.jpg');
+      myImage.setAttribute('src','/samochod-ortal.jpg');
     }
 }
 
